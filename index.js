745f0e52944d3ed63279811ca4a6e17919d40fc0
piyushraj2340 <piyushraj2340@gmail.com>
2020-08-14 16:09:29 +0530

    Update
    content update

--- index.js
+++ index.js
@@ -1,2 +1,93 @@
+var accLeft = document.getElementsByClassName("accordion-left");
+var accRight = document.getElementsByClassName("accordion-right");
 
+var i, panel;
 
+for (i = 0; i < accLeft.length; i++) {
+	accLeft[i].addEventListener("click", function() {
+		this.classList.toggle("active");
+		var panel = this.nextElementSibling;
+		if(panel.style.maxHeight) {
+			panel.style.maxHeight = null;
+		}
+		else {
+			panel.style.maxHeight = panel.scrollHeight + "px";
+		}
+	})
+}
+
+for (i = 0; i < accRight.length; i++) {
+	accRight[i].addEventListener("click", function() {
+		this.classList.toggle("active");
+		var panel = this.nextElementSibling;
+		if(panel.style.maxHeight) {
+			panel.style.maxHeight = null;
+		}
+		else {
+			panel.style.maxHeight = panel.scrollHeight + "px";
+		}
+	})
+}
+
+ 
+ function filterFunctionLeft() {
+	 var input, filter, button, txtValue, i;
+	 
+	 input = document.getElementById("search-left");
+	 filter = input.value.toUpperCase();
+	 butt = document.getElementsByClassName("accordion-left");
+	 
+	 
+	 
+	 for (i = 0; i < butt.length; i++) {
+		 txtValue = butt[i].textContent || butt[i].innerText;
+		 
+		 var panel = butt[i].nextElementSibling;
+		 butt[i].classList.remove("active");
+	 
+	     panel.style.maxHeight = null;
+		 
+		 if (txtValue.toUpperCase().indexOf(filter) > -1) {
+			 butt[i].style.display = "";
+		 }
+		 
+		 else {
+			 butt[i].style.display = "none";
+		 }
+	 }
+ }
+ 
+ 
+ function filterFunctionRight() {
+	 var input, filter, button, txtValue, i;
+	 
+	 input = document.getElementById("search-right");
+	 filter = input.value.toUpperCase();
+	 butt = document.getElementsByClassName("accordion-right");
+	 
+	 
+	 
+	 for (i = 0; i < butt.length; i++) {
+		 txtValue = butt[i].textContent || butt[i].innerText;
+		 
+		 var panel = butt[i].nextElementSibling;
+		 butt[i].classList.remove("active");
+	 
+	     panel.style.maxHeight = null;
+		 
+		 if (txtValue.toUpperCase().indexOf(filter) > -1) {
+			 butt[i].style.display = "";
+		 }
+		 
+		 else {
+			 butt[i].style.display = "none";
+		 }
+	 }
+ }
+ 
+ 
+ 
+ 
+ function menuFunction(x) {
+	 x.classList.toggle("change");
+ }
